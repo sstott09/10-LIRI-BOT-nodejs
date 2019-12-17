84d373c70df301a2da97f831d4f417bd2878e4ac
Steven Stot <sstott09@yahoo.com>
2019-12-16 22:27:16 -0700

    Update LIRI.bot

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# nodejs
+## University of Denver 
+## Class: DU_VIRT_FSF_PT_09_2019_U.O.MW
+## Program: Full Stack Flex Online Part-Time
+## 10- LIRI-node-app
+...........................................................................................................
+
+### _Language_ Interpretation and Recognition Interface (LIRI-Bot)
+
+### About
+In this assignment, I've created a LIRI bot. LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a _Language_ Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+### Objective
+
 
 1. Clearly state the problem the app is trying to solve (i.e. what is it doing and why)
 2. Give a high-level overview of how the app is organized
--- liri-node-app.js
+++ liri-node-app.js
@@ -1,9 +1,16 @@
+// Read and set environment variables
 require("dotenv").config();
 
+// VARS
+// var request = require("request");
+var fs = require("fs");
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+var axios = require("axios")
+var moment = require("moment")
 
+// VARS to capture user inputs
 var command = process.argv[2];
 var search = process.argv.slice(3).join(" ");
 
@@ -13,11 +20,26 @@ console.log("search term :" + search)
 
 var startProg = function (command, search) {
     switch (command) {
-        case "concert-this": getMyBands(search);
+        case "concert-this":
+            if (search == "") {
+                console.log("You need to search a band");
+            } else {
+                getMyBands(search);
+            }
             break;
-        case "spotify-this-song": getMeSpotify(search);
+        case "spotify-this-song":
+            if (search == "") {
+                getMeSpotify("Ace of Base - The Sign");
+            } else {
+                getMeSpotify(search);
+            }
             break;
-        case "movie-this": getMovie(search);
+        case "movie-this":
+            if (search == "") {
+                getMovie("Mr Nobody");
+            } else {
+                getMovie(search);
+            }
             break;
         case "do-what-it-says": doWhatItSays();
             break;
@@ -27,10 +49,9 @@ var startProg = function (command, search) {
 
 startProg(command, search);
 
+//Funtion for Song Info: Spotify
 function getMeSpotify(songName) {
-    if (songName == undefined) {
-        songName = "What's my age again"
-    }
+
     spotify.search(
         {
             type: 'track',
@@ -41,15 +62,55 @@ function getMeSpotify(songName) {
             if (err) {
                 return console.log('Error occurred: ' + err);
             }
-var songs  = data.tracks.items
-//console.log(JSON.stringify(songs))
-for(var i=0; i<songs.length; i++){
-    console.log("-----------------");
-    console.log("artist name:" +songs[i].artists[0].name);
-    console.log("song name: " +songs[i].name);
-    console.log("preview link:" +songs[i].preview_url);
-    console.log("album: " +songs[i].album.name);
-}
+            var songs = data.tracks.items
+            //console.log(JSON.stringify(songs))
+            for (var i = 0; i < songs.length; i++) {
+                console.log("-----------------");
+                console.log("artist name:" + songs[i].artists[0].name);
+                console.log("song name: " + songs[i].name);
+                console.log("preview link:" + songs[i].preview_url);
+                console.log("album: " + songs[i].album.name);
+            }
         });
+}
+//Funtion for Band Info: Bands in Town
+function getMyBands(bandNAME) {
+    var queryURL = "https://rest.bandsintown.com/artists/" + bandNAME + "/events?app_id=codingbootcamp";
+    axios.get(queryURL).then(function (res) {
+        //console.log(res)
+        const bandInfo = res.data;
+        for (var i = 0; i < 5; i++) {
+            console.log("Venue: " + bandInfo[i].venue.name + " " + bandInfo[i].venue.country + " " + bandInfo[i].venue.city);
+            console.log("date and Time: " + moment(bandInfo[i].datetime).format("MM/DD/YYYY"));
+            console.log("------------------------------------------------------");
 
+        }
+    })
+}
+
+//Funtion for Movie Info: OMDB
+function getMovie(movieName) {
+    var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
+    axios.get(urlHit).then(function (res) {
+        //console.log(res)
+        const movieInfo = res.data;
+        //console.log(movieInfo)
+        console.log("Movie Name: " + movieInfo.Title);
+        console.log("Released: " + movieInfo.Released);
+        console.log("Plot: " + movieInfo.Plot);
+        console.log("Actors: " + movieInfo.Actors);
+        console.log("IMDB rating: " + movieInfo.imdbRating)
+
+    })
+}
+
+//Funtion for Do What It Says Info: random.txt
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        console.log(data)
+        var dataArr = data.split(",");
+        console.log(dataArr);
+        startProg(dataArr[0], dataArr[1]);
+
+    })
 }
